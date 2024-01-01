a4e5e6e0f3426fd6670e4cdec796782a03ab1d2d
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-01-01 23:01:23 +0100

    update seeder with sizes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,58 +39,53 @@ class DatabaseSeeder extends Seeder
             'image' => 'https://cdn.laredoute.com/products/7/5/6/7564fcca940d6755f6c057a24d335891.jpg?width=1200&dpr=1',
             'price' => '759.00',
             'discount_price' => '529.00',
-            //sizes()->attach([$s90x190->id, $s140x190->id]),
         ]);
         $m1 -> sizes()->attach([$s90x190->id, $s140x190->id]);
 
         $m2 = Matelas::factory()->create([
             'name' => 'Matelas Assurance',
             'brand_id' => 3,
-            //'size_id' => 1,
             'image' => 'https://cdn.laredoute.com/products/5/1/7/517d0a25c5439c7a7626a9d4d25e3e33.jpg?width=1200&dpr=1',
             'price' => '809.00',
             'discount_price' => '709.00',
         ]);
         $m2 -> sizes()->attach([$s90x190->id, $s140x190->id]);
 
-        /* Matelas::factory()->create([
+        $m3 = Matelas::factory()->create([
             'name' => 'Matelas Essentiel',
             'brand_id' => 1,
-            //'size_id' => 2,
             'image' => 'https://cdn.laredoute.com/products/3/4/7/347fb06cc24d280c51b2050c04439136.jpg?width=1200&dpr=1',
             'price' => '759.00',
             'discount_price' => '529.00',
-            sizes()->attach($s140x190), //pas besoin de préciser l'id quand il n'y en a qu'un ?
         ]);
+        $m3 -> sizes()->attach($s140x190); //pas besoin de préciser "->id" quand il n'y en a qu'un
 
-        Matelas::factory()->create([
+       $m4 = Matelas::factory()->create([
             'name' => 'Matelas Prestige',
             'brand_id' => 4,
-            //'size_id' => 3,
             'image' => 'https://media.literieprivee.com/904-large_default/matelas-memoire-de-forme-bultex-body-up-160x200.jpg',
             'price' => '1019.00',
             'discount_price' => '0',
-            sizes()->attach($s160x200),
         ]);
+        $m4->sizes()->attach($s160x200);
 
-        Matelas::factory()->create([
+        $m5 = Matelas::factory()->create([
             'name' => 'Matelas Confort Ultime',
             'brand_id' => 4,
-            //'size_id' => 2,
             'image' => 'https://cdn.laredoute.com/products/8/c/d/8cdef6938ae5f3122fdaa6c6bc5cc6dc.jpg?width=1200&dpr=1',
             'price' => '999.00',
             'discount_price' => '0',
-            sizes()->attach($s140x190),
         ]);
+        $m5->sizes()->attach($s140x190);
 
-        Matelas::factory()->create([
+        $m6 = Matelas::factory()->create([
             'name' => 'Matelas Tout Doux',
             'brand_id' => 1,
-            //'size_id' => 2,
             'image' => 'https://cdn.laredoute.com/products/7/e/0/7e09e5b8ecec28cdd0f7d85c23a064cb.jpg?width=1200&dpr=1',
             'price' => '999.00',
             'discount_price' => '0',
-            sizes()->attach($s140x190),
-        ]); */
+            
+        ]);
+        $m6->sizes()->attach($s140x190);
     }
 }
